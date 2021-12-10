20265707c252d0f3bf5b5012a3155b18e723de7d
sunghwki <52474291+swkim12345@users.noreply.github.com>
2021-12-10 15:27:08 +0900

    add stock_lstm

---
+++ stock_lstm.py
@@ -0,0 +1,11 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+#0. 하이퍼 파라미터 설정하는 함수
+#1. 데이터 셋 불러오는 함수
+#2. 모델 구성하는 함수
+#3. 학습시키는 함수
+#4. 학습결과를 출력하는 함수
+#5. 임의의 값을 입력받아 y값을 예측하는 함수
+
+
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from keras.backend import truncated_normal
 from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
@@ -16,7 +17,8 @@ x = x.reshape(x.shape[0], x.shape[1], 1)
 
 #2 모델 구성
 model = Sequential()
-model.add(LSTM(10, activation = 'relu', input_shape = (3,1)))
+model.add(LSTM(10, activation = 'relu', input_shape = (3,1), return_sequences=True))
+model.add(LSTM(10, activation = "relu", return_sequences=True)) #연쇄적인 LSTM 레이어를 만들기 위해 return_sequences를 True로 만들어야 함
 
 model.add(Dense(5))
 model.add(Dense(1))
@@ -24,10 +26,11 @@ model.add(Dense(1))
 model.summary()
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(x, y, epochs=100, batch_size=1
+model.fit(x, y, epochs=100, batch_size=1, verbose=2)
 
 x_input = array([6, 7, 8])
 x_input = x_input.reshape((1,3,1))
 
 yhat = model.predict(x_input)
+model.summary()
 print(yhat)
