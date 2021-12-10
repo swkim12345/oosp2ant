5dfb1fa4256aea16a0dca8fbba827c461d86a3b7
sunghwki <52474291+swkim12345@users.noreply.github.com>
2021-12-10 15:44:47 +0900

    add stock_lstm

--- stock_lstm.py
+++ stock_lstm.py
@@ -1,11 +1,27 @@
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, LSTM
+
+
 
 #0. 하이퍼 파라미터 설정하는 함수
+def set_hyperparameter():
+	pass
 #1. 데이터 셋 불러오는 함수
+def load_dataset():
+	pass
 #2. 모델 구성하는 함수
+def make_model():
+	
+	pass
 #3. 학습시키는 함수
+def learn_model():
+	pass
 #4. 학습결과를 출력하는 함수
+def print_result():
+	pass
 #5. 임의의 값을 입력받아 y값을 예측하는 함수
+def predict_value():
+	pass
 
-
+if '__name__' == '__main__':
+	pass
