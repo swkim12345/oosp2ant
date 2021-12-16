d4ca972fb95d18f968d20039fea4b1a5b5a3c0f0
CJY-kor <vivo_reprise@naver.com>
2021-12-16 21:54:11 +0900

    stock_environment, stock_agent 최신화 2016-12-16

--- stock_agent.py
+++ stock_agent.py
@@ -1,2 +1,34 @@
+import random
+import numpy as np
+
+
 class StockAgent():
     def __init__(self):
+        """
+        Q network 와 target Q network 를 정의해야됨
+        qnet
+        qnet_target
+
+        Replay Buffer 을 사용해야됨
+
+        Epsilon 값 또한 정의해야됨
+        """
+        self.eps = 0.9
+
+    def select_action(self, state):
+        """
+        epsilon greedy 를 이용하여 action을 정함
+        """
+
+        coin = random.random()
+
+        if coin < self.eps:  # epsilon - greedy
+            random_action = random.randint(-1, 1)
+            return random_action
+        else:
+            # 높은 q value 를 가지는 action 을 택함.
+
+
+
+        
+
--- stock_environment.py
+++ stock_environment.py
@@ -4,15 +4,25 @@ import datetime
 class StockWorld():
     """
     강화학습의 Environment 에 해당되는 부분입니다.
+
+    구현 내용
+
+    - __init__ : 상태공간 정의
+    - step(self, action) : action 에 따른 reward 와 다음 state 를 return 해주는 함수
+    - is_done(self) : 에피소드가 끝났는 지 확인해 주는 함수
+    - reset() : Environment 를 초기화 해주는 함수
+    - current_state : 현재 상태를 return 해주는 함수
     """
+
     def __init__(self, time):
         """
         state 를 구현함
         :param time: 현재 시간
         """
+        self.destination = datetime.date(2019, 1, 1)
         self.time = time
         self.market_feature = [[0 for _ in range(4)] for _ in range(30)]  # m, n 행렬로 표현한 시장지표
-        self.asset = 0  # 자산 보유 현황 : 0 없음, 1 있음
+        self.asset = [0, 1000]  # index 0 : 현금, index 1 : ETF 펀드 (kospi * 1000)
 
     def step(self, action):
         """
@@ -20,27 +30,54 @@ class StockWorld():
         # action 0 : 보유 유지, action -1 : 팔기, action 1 : 사기
         """
 
-        # action 0 일 경우 aseet 변화 없음 -> 아무것도 안 함
+        self.time = self.time + datetime.timedelta(days=1)
 
-        if action == -1:
-            if self.asset == 1:  # action -1 : 자산이 있을경우 자산이 없게 됨, 자산이 없을경우 아무것도 안 함
-                self.asset = 0
+        # market feature 다음 시간의 market feature 로 데이타 베이스에서 받아 최신화 시켜야 됨
+
+        current_asset = sum(self. asset)
+
+        if action == 0:  # 현금일 경우 아무 것도 안함, ETF 펀드일 경우 가치 최신화
+            if self.asset[1] != 0:
+                self.asset[1] = self.asset[1]  # 가치 최신화
+        elif action == -1:
+            if self.asset[1] != 0:  # ETF 펀드가 존재할 경우 현금으로 전환 수수료 계산 해주어야 됨
+                self.asset[0] = self.asset[1]
+                self.asset[1] = 0
         elif action == 1:
-            if self.asset == 0:  # action 1 : 자산이 없을경우 자산이 있게 됨, 자산이 있을경우 아무것도 안 함
-                self.asset = 1
+            if self.asset[0] != 0:  # 현금이 존재 할 경우 ETF 로 전환
+                self.asset[1] = self.asset[0]  # 가치가 최신화 된 ETF 임 수수료 계산 해주어야 됨
+                self.asset[0] = 0
 
-        self.time = datetime.datetime(self.time.year, self.time.month, self.time.day + 1)
+        reward = (sum(self.asset) - current_asset) / current_asset
 
+        done = self.is_done()
+
+        return (self.time, self.market_feature, self.asset), reward, done
+
+    def is_done(self):
+        if self.time == self.destination:
+            return True
+        else:
+            return False
+
+    def reset(self, time):
+        self.time = time
+        # 나머지 state 값들도 초기화 해야 됨
+        
     def current_state(self):
-        print(self.time)
-        print(self.market_feature)
-        print(self.asset)
+        """
+        현재 상태를 리턴한다.
+        """
+        return self.time, self.market_feature, self.asset
 
 
-current_time = datetime.datetime(2018, 11, 10)
+current_time = datetime.date(2018, 11, 10)
 
 test1 = StockWorld(current_time)
 test1.current_state()
 
-test1.step(1)
-test1.current_state()
+print(test1.step(-1))
+print(test1.step(1))
+
+while not test1.step(0)[2]:
+    print(test1.current_state())
