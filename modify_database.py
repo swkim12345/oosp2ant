68ae367c4626761dc06bfb3c22278fa330535cef
sunghwki <52474291+swkim12345@users.noreply.github.com>
2021-12-17 02:53:20 +0900

    modify data

--- modify_database.py
+++ modify_database.py
@@ -1,54 +1,87 @@
-import sqlite3 as sql
+import sqlite3
 import time
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import numpy as np
+#import numpy as np
 
-def kospi(conn, cur):
-	date = datetime.strptime('2000/01/01', '%Y/%m/%d')
-	for i in range(7669):
-		s = '{0}/{1:0>2}/{2:0>2}'.format(date.year, date.month, date.day)
-		
-		select = "select * from kospi where 일자 = ?"
-		cur.execute(select, ("s", ))
-		rows = cur.fetchall()
+'''
+이 함수는 처음에만 실행하게 됨.
+더이상 실행할 이유 없음.
+사용하지 마세요!
+'''
+#끝나는 날짜
 
-		for i in rows:
-			print(i)
+# 36526 ~ 44530까지의 시간을 가짐
 
-	conn.commit()
+# def kospi(conn, cur):
+# 	'''
+# 	단순 노가다로 복사해주는 함수(date 의 unique가 보장되어 있으므로 이러한 방법을 사용함.)
+# 	'''
+# 	date_max = 36526
+# 	date = 36529
+# 	index_max = date_max - date
+# 	for i in range(0, index_max, -1):
+# 		select = "select * from kospi where date = ?"
+# 		cur.execute(select, (str(date + i) + ".0", ))
+# 		rows = cur.fetchall() #작동 잘 됨
 
+# 		print(rows[0][0])
 
-def kospiInit(conn, cur):
-	addCol = "insert into kospi values(?, ?, ?, ?, ?)"
-	cur.execute(addCol, ("2000/01/01", "1,059.04", "3.01", "195,899", "0.00"))
-	cur.execute(addCol, ("2000/01/02", "1,059.04", "3.01", "195,899", "0.00"))
-	cur.execute(addCol, ("2000/01/03", "1,059.04", "3.01", "195,899", "0.00"))
+# 		insert_row = "insert or ignore into kospi (date, end, stockper, trading, percent) values(?, ?, ?, ?, ?)"
+# 		cur.execute(insert_row, (str(date + i - 1) + ".0", rows[0][1], rows[0][2], rows[0][3], rows[0][4]))
 
-def nasdaqInit(conn,cur):
-	addCol = "insert into nasdaq values(?, ?, ?, ?, ?, ?, ?)"
-	cur.execute(addCol, ("2000년 01월 01일", "4,131.1", "4,186.2", "4,192.2", "3,989.7", "-", "1.52%"))
-	cur.execute(addCol, ("2000년 01월 02일", "4,131.1", "4,186.2", "4,192.2", "3,989.7", "-", "1.52%"))
+# 	conn.commit()
 
-	conn.commit()
+# def nasdaq(conn, cur):
+# 	#최대 : 44530, 최소 :36528
+# 	date_max = 36526
+# 	date = 36528
+# 	index_max = date_max - date
+# 	for i in range(0, index_max, -1):
+# 		select = "select * from nasdaq where date = ?"
+# 		cur.execute(select, (str(date + i) + ".0", ))
+# 		rows = cur.fetchall() #작동 잘 됨
 
-def spInit(conn, cur):
-	addCol = "insert into sp values(?, ?, ?, ?, ?, ?, ?)"
-	cur.execute(addCol, ("Jan 02, 2000", "145.44", "148.25", "148.25", "143.88", "8.16M", "-0.98%"))
-	cur.execute(addCol, ("Jan 01, 2000", "145.44", "148.25", "148.25", "143.88", "8.16M", "-0.98%"))
+# 		print(rows[0][0])
+
+# 		insert_row = "insert or ignore into nasdaq (date, end, open, high, low, frequency, percent) values(?, ?, ?, ?, ?, ?, ?)"
+# 		cur.execute(insert_row, (str(date + i - 1) + ".0", rows[0][1], rows[0][2], rows[0][3], rows[0][4], rows[0][5], rows[0][6]))
+
+# 	conn.commit()
+
+def sp(conn, cur):
+	date_max = 36526
+	date = 36528
+	index_max = date_max - date
+	for i in range(0, index_max, -1):
+		select = "select * from sp where date = ?"
+		cur.execute(select, (str(date + i) + ".0", ))
+		rows = cur.fetchall() #작동 잘 됨
+
+		print(rows[0][0])
+
+		insert_row = "insert or ignore into sp (date, end, open, high, low, trading, percent) values(?, ?, ?, ?, ?, ?, ?)"
+		cur.execute(insert_row, (str(date + i - 1) + ".0", rows[0][1], rows[0][2], rows[0][3], rows[0][4], rows[0][5], rows[0][6]))
 
 	conn.commit()
 
 if __name__=="__main__":
-	conn = sql.connect("data.sqlite")
+	conn = sqlite3.connect("data.db")
 
 	cur = conn.cursor()
 
-	#kospiInit(conn, cur)
-	#nasdaqInit(conn, cur)
-	#spInit(conn, cur)
 
-	kospi(conn,cur)
+	#kospi(conn,cur)
+
+	#nasdaq(conn, cur)
+
+	sp(conn, cur)
+
+	# sql = "select date from kospi order by date desc"
+	# cur.execute(sql)
+	# rows = cur.fetchall()
+	# print(rows)
+
 
 	cur.close()
 	conn.close()
