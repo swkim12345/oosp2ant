2547b97d93a3c104a29ee66bbeb342ef7efcfb20
sunghwki <52474291+swkim12345@users.noreply.github.com>
2021-12-16 18:51:50 +0900

    modify sqlite

--- modify_database.py
+++ modify_database.py
@@ -1,4 +1,15 @@
 import sqlite3 as sql
-import numpy as np
+#import numpy as np
 
+conn = sql.connect("data.sqlite")
 
+cur = conn.cursor()
+
+cur.execute("select * from kospi")
+rows = cur.fetchall()
+
+for i in rows:
+	print(i)
+
+conn.commit()
+cur.close()
