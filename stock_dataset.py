1546d4273656f12da31d898e37011c44d272093c
CJY-kor <vivo_reprise@naver.com>
2021-12-17 15:56:41 +0900

    the real kospi 수정 2016-12-17 15:56

--- stock_dataset.py
+++ stock_dataset.py
@@ -3,127 +3,133 @@ from typing import MutableMapping
 import numpy as np
 import datetime
 
+
 # 36526 ~ 44530까지의 시간을 가짐
 class stock_dataset():
-	'''
+    '''
 	float np로 정제된 dataset을 받아올 수 있는 클래스
 	kospi, nasdaq, sp로 구성되어 있음.
 	'''
-	K = 1000
-	M = 1000000
-	B = 1000000000
+    K = 1000
+    M = 1000000
+    B = 1000000000
 
-	#filename을 받아 데이터베이스를 열게 됨.
-	def __init__(self, filename):
-		self.filename = filename
+    # filename을 받아 데이터베이스를 열게 됨.
+    def __init__(self, filename):
+        self.filename = filename
 
+    # K / M / B을 변환해주는 함수
+    def mulAbbreviation(self, floatAbb):
+        floatReal = 0.0
 
-	#K / M / B을 변환해주는 함수
-	def mulAbbreviation(self, floatAbb):
-		floatReal = 0.0
+        if 'K' in floatAbb:
+            floatReal = float(floatAbb[0:-1]) * self.K
+        elif 'M' in floatAbb:
+            floatReal = float(floatAbb[0:-1]) * self.M
+        elif 'B' in floatAbb:
+            floatReal = float(floatAbb[0:-1]) * self.B
+        return floatReal
 
-		if 'K' in floatAbb:
-			floatReal = float(floatAbb[0:-1]) * self.K
-		elif 'M' in floatAbb:
-			floatReal = float(floatAbb[0:-1]) * self.M
-		elif 'B' in floatAbb:
-			floatReal = float(floatAbb[0:-1]) * self.B
-		return floatReal
+    def kospi(self):
+        # db 핸들러 받아옴
+        conn = sqlite3.connect(self.filename)
+        cur = conn.cursor()
 
-	def kospi(self):
-		#db 핸들러 받아옴
-		conn = sqlite3.connect(self.filename)
-		cur = conn.cursor()
+        # SQL에 쿼리를 날려 Kospi 내의 모든 것을 받아옴
+        sql = "select * from kospi order by date asc"
+        cur.execute(sql)
+        rows = cur.fetchall()
 
-		#SQL에 쿼리를 날려 Kospi 내의 모든 것을 받아옴
-		sql = "select * from kospi order by date asc"
-		cur.execute(sql)
-		rows = cur.fetchall()
+        # np 형태로 변환
+        kospi_np = np.array(rows, dtype='float32')
 
-		#np 형태로 변환
-		kospi_np = np.array(rows, dtype='float32')
+        cur.close()
+        conn.close()
+        return kospi_np
 
-		cur.close()
-		conn.close()
-		return kospi_np
+    # 위 함수와 다를게 없음
+    def nasdaq(self):
+        conn = sqlite3.connect(self.filename)
+        cur = conn.cursor()
 
-	#위 함수와 다를게 없음
-	def nasdaq(self):
-		conn = sqlite3.connect(self.filename)
-		cur = conn.cursor()
+        sql = "select * from nasdaq order by date asc"
 
-		sql = "select * from nasdaq order by date asc"
+        cur.execute(sql)
+        rows = cur.fetchall()
 
-		cur.execute(sql)
-		rows = cur.fetchall()
+        for i in range(len(rows)):
+            rows[i] = list(rows[i])
+            rows[i][5] = self.mulAbbreviation(rows[i][5])
 
-		for i in range(len(rows)):
-			rows[i] = list(rows[i])
-			rows[i][5] = self.mulAbbreviation(rows[i][5])
+        nasdaq_np = np.array(rows, dtype='float32')
 
-		nasdaq_np = np.array(rows, dtype='float32')
+        cur.close()
+        conn.close()
+        return nasdaq_np
 
-		cur.close()
-		conn.close()
-		return nasdaq_np
+    def sp(self):
+        conn = sqlite3.connect(self.filename)
+        cur = conn.cursor()
 
-	def sp(self):
-		conn = sqlite3.connect(self.filename)
-		cur = conn.cursor()
+        sql = "select * from sp order by date asc"
 
-		sql = "select * from sp order by date asc"
+        cur.execute(sql)
+        rows = cur.fetchall()
 
-		cur.execute(sql)
-		rows = cur.fetchall()
+        for i in range(len(rows)):
+            rows[i] = list(rows[i])
+            rows[i][5] = self.mulAbbreviation(rows[i][5])
 
-		for i in range(len(rows)):
-			rows[i] = list(rows[i])
-			rows[i][5] = self.mulAbbreviation(rows[i][5])
+        sp_np = np.array(rows, dtype='float32')
 
-		sp_np = np.array(rows, dtype='float32')
+        cur.close()
+        conn.close()
+        return sp_np
 
+    def tech_indi(self):
+        conn = sqlite3.connect(self.filename)
+        cur = conn.cursor()
 
-		cur.close()
-		conn.close()
-		return sp_np
+        kospi = "select date, stockper, percent from kospi order by date asc"
 
-	def tech_indi(self):
-		conn = sqlite3.connect(self.filename)
-		cur = conn.cursor()
+        cur.execute(kospi)
+        kospi_rows = np.array(cur.fetchall(), dtype='float32')
 
-		kospi = "select date, stockper, percent from kospi order by date asc"
+        nasdaq = "select percent * 100 from nasdaq order by date asc"
 
-		cur.execute(kospi)
-		kospi_rows = np.array(cur.fetchall(), dtype='float32')
+        cur.execute(nasdaq)
+        nasdaq_rows = np.array(cur.fetchall(), dtype='float32')
 
-		nasdaq = "select percent * 100 from nasdaq order by date asc"
+        sp = "select percent * 100 from sp order by date asc"
 
-		cur.execute(nasdaq)
-		nasdaq_rows = np.array(cur.fetchall(), dtype='float32')
+        cur.execute(sp)
+        sp_rows = np.array(cur.fetchall(), dtype='float32')
 
-		sp = "select percent * 100 from sp order by date asc"
+        tech_indi_np = np.concatenate((kospi_rows, nasdaq_rows, sp_rows), axis=1)
 
-		cur.execute(sp)
-		sp_rows = np.array(cur.fetchall(), dtype='float32')
+        return tech_indi_np
 
-		tech_indi_np = np.concatenate((kospi_rows, nasdaq_rows, sp_rows), axis=1)
+    def data_modification(self):  # tech_indi 에서 날짜를 datetime 형식으로 바꿈
+        start = datetime.date(2000, 1, 1)
 
-		return tech_indi_np
+        stock_data = self.tech_indi()
 
-	def data_modification(self):  # tech_indi 에서 날짜를 datetime 형식으로 바꿈
-		start = datetime.date(2000, 1, 1)
+        list_data = list(stock_data)
 
-		stock_data = self.tech_indi()
+        for i in range(len(list_data)):
+            list_data[i] = list(list_data[i])
 
-		list_data = list(stock_data)
+        for i in range(len(list_data)):
+            list_data[i][0] = start
+            start += datetime.timedelta(days=1)
 
-		for i in range(len(list_data)):
-			list_data[i] = list(list_data[i])
+        return list_data
 
-		for i in range(len(list_data)):
-			list_data[i][0] = start
-			start += datetime.timedelta(days=1)
+    def real_kospi(self):
 
-		return list_data
-
-
+        conn = sqlite3.connect(self.filename)
+        cur = conn.cursor()
+        kospi = "select end from kospi order by date asc"
+        cur.execute(kospi)
+        kospi_rows = np.array(cur.fetchall(), dtype='float32')
+        return kospi_rows
