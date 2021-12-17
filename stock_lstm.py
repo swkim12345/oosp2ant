09264e86a999d47e0592026fe160ee9f79710024
sunghwki <52474291+swkim12345@users.noreply.github.com>
2021-12-18 02:46:06 +0900

    add

--- stock_lstm.py
+++ stock_lstm.py
@@ -9,64 +9,15 @@ class Model():
 
     def __init__(self):
 
-        self.lstm_encoder = Sequential([
-            LSTM(input_shape=[30, 4], units=16, return_sequences=True),
-            LSTM(units=30)
-        ])  # 케라스 순차 모델 사용
+        input_1 = Input(shape=(30, 4), name='input_1')
+        input_2 = Input(shape=(2), name='input_2')
 
-        self.lstm_decoder = Sequential([
-            RepeatVector(30, input_shape=[30]),
-            LSTM(units=30, return_sequences=True),
-            LSTM(units=16, return_sequences=True),
-            TimeDistributed(Dense(4, activation="relu"))
-        ])
+        hidden1_lstm = LSTM(30, return_sequences=False)(input_1)
 
-        self.lstm_ae = Sequential([self.lstm_encoder, self.lstm_decoder])
-        self.lstm_ae.compile(optimizer='adam', loss='mse')
-        #일단 주석처리함.
-        self.lstm_encoder.summary()
-        self.lstm_decoder.summary()
-        self.lstm_ae.summary()
+        concat = Concatenate()([input_2, hidden1_lstm])
 
-    def fit_encoder_model(self, X_train, X_valid):
-        history = self.lstm_ae.fit(X_train, X_train, epochs=50, validation_data=(X_valid, X_valid))
+        output = Dense(3, name='sum_output')(concat)
 
-    def ret_encoder_model(self):
-        return self.lstm_encoder
-
-
-
-# class Model():
-
-#     def __init__(self):
-
-#         self.lstm_encoder = Sequential([
-#             LSTM(input_shape=[30, 4], units=32, activation='relu', return_sequences=True),
-#             LSTM(units=30)
-#         ])  # 케라스 순차 모델 사용
-
-#         input_1 = keras.layers.Input(shape=[2])
-#         input_2 = keras.layers.Input(shape=[30, 4])
-#         encoder = self.lstm_encoder(input_2)
-
-#         input_1_concat_input_2 = keras.layers.Concatenate(axis=1)([input_1, encoder])
-
-#         self.lstm_decoder = Sequential([
-#             RepeatVector(32, input_shape=[32]),
-#             LSTM(units=20, return_sequences=True),
-#             LSTM(units=32, return_sequences=True),
-#             TimeDistributed(Dense(4, activation="sigmoid"))
-#         ])
-
-#         output = self.lstm_decoder(input_1_concat_input_2)
-
-#         self.lstm_ae = keras.Model(inputs = [input_1, input_2], outputs = [output])
-#         self.lstm_ae.compile(optimizer='adam', loss='mse')
-#         #일단 주석처리함.
-#         self.lstm_encoder.summary()
-#         self.lstm_decoder.summary()
-#         self.lstm_ae.summary()
-
-#     def fit_model(self, X_train, X_valid):
-#         history = self.lstm_ae.fit(X_train, X_train, epochs=50, validation_data=(X_valid, X_valid))
-
+        self.model = keras.Model(inputs=[input_1, input_2], outputs = [output])
+        self.model.compile(optimizer='adam', loss='mse')
+        self.model.summary()
